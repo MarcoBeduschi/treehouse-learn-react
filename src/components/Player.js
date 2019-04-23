7cd4cf15af772b79a2846e4589672a0a23fabc79
Marco Beduschi <beduschimarco@gmail.com>
2019-04-23 12:53:40 -0300

    wip

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import Counter from './Counter';
 
-const Player = (props) => {
-  return (
-    <div className="player">
-      <span className="player-name">
-        <button className="remove-player" onClick={() => props.removePlayer(props.id)}>✖</button>
-        { props.name }
-      </span>
+class Player extends PureComponent {
 
-      <Counter
-        score={props.score}
-        index={props.index}
-        changeScore={props.changeScore}
-      />
-    </div>
-  );
+  static PropTypes = {
+    changeScore: PropTypes.func,
+    removePlayer: PropTypes.func,
+    name: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    score: PropTypes.number.isRequired,
+    index: PropTypes.number.isRequired
+  }
+  
+  render() {
+    const {
+      id,
+      index,
+      name,
+      score,
+      removePlayer,
+      changeScore
+    } = this.props
+
+    return (
+      <div className="player">
+        <span className="player-name">
+          <button className="remove-player" onClick={() => removePlayer(id)}>✖</button>
+          { name }
+        </span>
+  
+        <Counter
+          score={score}
+          index={index}
+          changeScore={changeScore}
+        />
+      </div>
+    );
+  }
 }
 
 export default Player;
